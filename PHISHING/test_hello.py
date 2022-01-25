ad5a9ae89a1889359f2ce2bd160cb3f78dbd1987
Shiva Krishna Reddy <shiva.reddy@thirdpresence.com>
2022-01-25 16:44:51 +0530

    sample test cases

--- PHISHING/test_hello.py
+++ PHISHING/test_hello.py
@@ -1,5 +1,6 @@
 from app import app
 import pytest
+from flask import Flask, request
 
 #import create_app
 from db import init_db
@@ -21,6 +22,7 @@ def test_app1():
     response = app.test_client().get('/')
     assert response.data == b'<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="utf-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n    \n    <!-- SEO Meta Tags -->\n    <meta name="description" content="Your description">\n    <meta name="author" content="Your name">\n\n    <!-- OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn -->\n\t<meta property="og:site_name" content="" /> <!-- website name -->\n\t<meta property="og:site" content="" /> <!-- website link -->\n\t<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->\n\t<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->\n\t<meta property="og:image" content="" /> <!-- image link, make sure it\'s jpg -->\n\t<meta property="og:url" content="" /> <!-- where do you want your post to link to -->\n\t<meta name="twitter:card" content="summary_large_image"> <!-- to have large image post format in Twitter -->\n\n    <!-- Webpage Title -->\n    <title>Intro</title>\n    \n    <!-- Styles -->\n    <link rel="preconnect" href="https://fonts.gstatic.com">\n    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600&display=swap" rel="stylesheet">\n    <link href="static/css/bootstrap.css" rel="stylesheet">\n    <link href="static/css/fontawesome-all.css" rel="stylesheet">\n    <link href="static/css/swiper.css" rel="stylesheet">\n\t<link href="static/css/magnific-popup.css" rel="stylesheet">\n\t<link href="static/css/styles.css" rel="stylesheet">\n\t\n\t<!-- Favicon  -->\n    <link rel="icon" href="static/images/favicon.png">\n</head>\n<body data-spy="scroll" data-target=".fixed-top">\n    \n    <!-- Navigation -->\n    <nav class="navbar navbar-expand-lg fixed-top navbar-light">\n        <div class="container">\n            \n            <!-- Text Logo - Use this if you don\'t have a graphic logo -->\n            <!-- <a class="navbar-brand logo-text page-scroll" href="index.html">Lomar</a> -->\n\n            <!-- Image Logo -->\n            <a class="navbar-brand logo-image" href="/"><img src="../static/images/download.svg" alt="alternative"></a> \n\n            <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">\n                <span class="navbar-toggler-icon"></span>\n            </button>\n\n            <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">\n                <ul class="navbar-nav ml-auto">\n                    <li class="nav-item">\n                        <a class="nav-link page-scroll" href="/">Home <span class="sr-only">(current)</span></a>\n                    </li>\n                    <li class="nav-item">\n                        <a class="nav-link page-scroll" href="/logon">SignUp</a>\n                    </li>\n                    \n                </ul>\n                <span class="nav-item social-icons">\n                    <span class="fa-stack">\n                        <a href="#your-link">\n                            <i class="fas fa-circle fa-stack-2x"></i>\n                            <i class="fab fa-facebook-f fa-stack-1x"></i>\n                        </a>\n                    </span>\n                    <span class="fa-stack">\n                        <a href="#your-link">\n                            <i class="fas fa-circle fa-stack-2x"></i>\n                            <i class="fab fa-twitter fa-stack-1x"></i>\n                        </a>\n                    </span>\n                </span>\n            </div> <!-- end of navbar-collapse -->\n        </div> <!-- end of container -->\n    </nav> <!-- end of navbar -->\n    <!-- end of navigation -->\n\n\n    <!-- Header -->\n    <header class="ex-header">\n        <div class="container">\n            <div class="row">\n                <div class="col-xl-10 offset-xl-1">\n                    <h1>PHISHING E-MAILS DETECTION</h1>\n                    <br>\n                    <a class="btn-solid-reg mb-5" href="/logon">SignUp</a>\n                </div> <!-- end of col -->\n               \n            </div> <!-- end of row -->\n        </div> <!-- end of container -->\n    </header> <!-- end of ex-header -->\n    <!-- end of header -->\n\n\n    <!-- Basic -->\n    <div class="ex-basic-1 pt-5 pb-5">\n        <div class="container">\n            <div class="row">\n                <div class="col-lg-12">\n                    <img class="img-fluid mt-5 mb-3" src="static/images/1.jpg" width="1110" alt="alternative">\n                </div> <!-- end of col -->\n            </div> <!-- end of row -->\n        </div> <!-- end of container -->\n    </div> <!-- end of ex-basic-1 -->\n    <!-- end of basic -->\n\n\n    \n\n    \n\n\n    <!-- Footer -->\n    <div class="footer">\n        <div class="container">\n             <!-- Copyright -->\n    <div class="copyright">\n        <div class="container">\n            <div class="row">\n                <div class="col-lg-12">\n                    <p class="p-small">Copyright \xc2\xa9 <a href="#your-link">2021</a></p>\n                </div> <!-- end of col -->\n            </div> <!-- enf of row -->\n        </div> <!-- end of container -->\n    </div> <!-- end of copyright --> \n    <!-- end of copyright -->\n        </div> <!-- end of container -->\n    </div> <!-- end of footer -->  \n    <!-- end of footer -->\n\n\n   \n    \n    \t\n    <!-- Scripts -->\n    <script src="static/js/jquery.min.js"></script> <!-- jQuery for Bootstrap\'s JavaScript plugins -->\n    <script src="static/js/bootstrap.min.js"></script> <!-- Bootstrap framework -->\n    <script src="static/js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->\n    <script src="static/js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->\n    <script src="static/js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->\n    <script src="static/js/scripts.js"></script> <!-- Custom scripts -->\n</body>\n</html>'
     
+
 def test_app2():
     response = app.test_client().get('/logon')
     print(response.data)
@@ -54,10 +56,10 @@ def test_valid_signin_logout(test_client):
     THEN check the response is valid
     """
     response = test_client.post('/login',
-                                data=dict(email='shivakrishnareddy.si4@gmail.com', password='FlaskIsAwesome'),
+                                data=dict(email='patkennedy79@gmail.com', password='FlaskIsAwesome'),
                                 follow_redirects=True)
     assert response.status_code == 200
-    assert b'Thanks for logging in, shivakrishnareddy.si4@gmail.com!' in response.data
+    assert b'Thanks for logging in, patkennedy79@gmail.com!' in response.data
     assert b'Flask User Management' in response.data
     assert b'Logout' in response.data
     assert b'Login' not in response.data
@@ -65,10 +67,56 @@ def test_valid_signin_logout(test_client):
 
 
 
+def test_predict():
+    response = app.test_client().get('/predict')
+    print(response.data)
+    expected_response = test_case['expected_response']
+    expected_status_code = test_case['expected_status_code']
+    assert response.status_code == 500#my_prediction
+    #assert response.data == b'<!DOCTYPE html>'
+    assert response.status_code == expected_status_code
+    #assert json.loads(response.data) == expected_response
+'''
+
+    with app.test_client() as client:
+        for test_case in test_data:
+            features = test_case['features']
+            expected_response = test_case['expected_response']
+            expected_status_code = test_case['expected_status_code']
+            # Test client uses "query_string" instead of "params"
+            response = client.get('/predict', query_string=features)
+            # Check that we got the correct status code back.
+            assert response.status_code == expected_status_code
+            # response.data returns a byte array, convert to a dict.
+            assert json.loads(response.data) == expected_response
+'''
 
 
 
-    """
+
+"""
+
+    with app.test_client() as client:
+        for test_case in test_data:
+            features = test_case['features']
+            expected_response = test_case['expected_response']
+            expected_status_code = test_case['expected_status_code']
+            # Test client uses "query_string" instead of "params"
+            response = client.get('/predict', query_string=features)
+            # Check that we got the correct status code back.
+            assert response.status_code == expected_status_code
+            # response.data returns a byte array, convert to a dict.
+            assert json.loads(response.data) == expected_response
+
+
+
+
+with app.test_request_context('/?name=Peter'):
+    assert request.path == '/'
+    assert request.args['name'] == 'Peter'
+
+
+
     GIVEN a Flask application configured for testing
     WHEN the '/logout' page is requested (GET)
     THEN check the response is valid
